48eea1c1734ad452a7fb2bdad25db377418bfe0e
Pati <bhagyaraju.pati@zoetis.com>
2024-12-10 18:27:10 +0530

    changed hosts to genera;

--- backend/backend/deployment.py
+++ backend/backend/deployment.py
@@ -2,8 +2,8 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['*']
 DEBUG=True
 SECRET_KEY = os.environ['MY_SECRET_KEY']
 
